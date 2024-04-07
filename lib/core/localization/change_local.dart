38f022daf6a25231dc93112c4b74ca2eba921ad7
Ahmed abdelnadi <93190015+am7405782@users.noreply.github.com>
2024-04-07 22:16:24 +0200

    Add LocaleController and MyServices classes
    Added LocaleController class for handling localization changes and MyServices class
    for managing shared preferences and service initialization. Also added initialServices
    function for initializing services asynchronously.

--- lib/core/localization/change_local.dart
+++ lib/core/localization/change_local.dart
@@ -1,35 +1,36 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/core/constant/theam-data.dart';
-// import 'package:flutter_application_1/core/service/my_service.dart';
-// import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-// class LocaleController extends GetxController {
-//   Locale? language;
-//   MyService myService = Get.find();
-//   ThemeData appTheme = englisTheam;
+import 'package:get/get.dart';
+import 'package:shoping_app/core/constant/theam-data.dart';
+import 'package:shoping_app/core/services/service.dart';
 
-//   changeLocal(String langCode) {
-//     Locale local = Locale(langCode);
-//     myService.sharedPreferences.setString("lang", langCode);
-//     appTheme = langCode == "ar" ? aradicTheam : englisTheam;
-//     Get.updateLocale(local);
-//     Get.changeTheme(appTheme);
-//   }
+class LocaleController extends GetxController {
+  Locale? language;
+  MyServices myService = Get.find();
+  ThemeData appTheme = englisTheam;
 
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     var sharedprefLag = myService.sharedPreferences.getString("lang");
-//     if (sharedprefLag == "ar") {
-//       language = const Locale("ar");
-//       appTheme = aradicTheam;
-//     } else if (sharedprefLag == "en") {
-//       language = const Locale("en");
-//       appTheme = englisTheam;
-//     }
-//     {
-//       language = Locale(Get.deviceLocale!.languageCode);
-//       appTheme = englisTheam;
-//     }
-//   }
-// }
+  changeLocal(String langCode) {
+    Locale local = Locale(langCode);
+    myService.sharedPreferences.setString("lang", langCode);
+    appTheme = langCode == "ar" ? aradicTheam : englisTheam;
+    Get.updateLocale(local);
+    Get.changeTheme(appTheme);
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    var sharedprefLag = myService.sharedPreferences.getString("lang");
+    if (sharedprefLag == "ar") {
+      language = const Locale("ar");
+      appTheme = aradicTheam;
+    } else if (sharedprefLag == "en") {
+      language = const Locale("en");
+      appTheme = englisTheam;
+    }
+    {
+      language = Locale(Get.deviceLocale!.languageCode);
+      appTheme = englisTheam;
+    }
+  }
+}
